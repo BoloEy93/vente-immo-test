2ff61804b7a5aa75acf3a3dae19ae51126cb46b4
Boris Ey. <67276238+BoloEy93@users.noreply.github.com>
2024-08-27 10:35:26 +0100

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 const Search = () => {
     return (
         <header>
-            <h2 className="header__title">Cherche, Explore et Achetes !.</h2>
+            <h2 className="header__title">Cherche, Explore et Achetes !</h2>
             <input
                 type="text"
                 className="header__search"
